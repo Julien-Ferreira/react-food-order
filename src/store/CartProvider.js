78a97a71ddd8a5a66e269084289353353729a0b6
Julien Fereira <ferreira-julien@live.fr>
2023-01-10 19:38:08 +0100

    state

---
+++ src/store/CartProvider.js
@@ -0,0 +1,21 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.provider value={cartContext}>
+      {props.children}
+    </CartContext.provider>
+  );
+};
+export default CartProvider;
